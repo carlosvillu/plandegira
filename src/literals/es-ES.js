f9f1b9433c49aceb998a874350f42ded96a4543f
Carlos Villuendas <carlosvillu@gmail.com>
2017-08-23 00:52:28 +0200

    feat(literals): first literals

--- src/literals/es-ES.js
+++ src/literals/es-ES.js
@@ -1,5 +1,8 @@
 export default {
   CHANNEL_TITLE: 'Canales',
+  EMAIL_PLACEHOLDER: 'usario@servidor.com',
+  NAME_PLACEHOLDER: 'Broda',
   NO_ITEMS: 'No hay elementos',
+  PASSWORD_PLACEHOLDER: 'más de 6 caracteres',
   USER_TITLE: 'Usuarios'
 }
