b89a28253da0b9ad20b5a9d0c324ef440779a6c5
Carlos Villuendas <carlosvillu@gmail.com>
2017-08-23 00:56:00 +0200

    feat(app): improve entry point

--- src/app.js
+++ src/app.js
@@ -6,54 +6,39 @@ import {register} from '@schibstedspain/sui-bundler/registerServiceWorker'
 import {Provider} from '@schibstedspain/ddd-react-redux'
 import {AppContainer} from 'react-hot-loader'
 import Router from 'react-router/lib/Router'
-import match from 'react-router/lib/match'
 import routes from './routes'
 import browserHistory from 'react-router/lib/browserHistory'
 import Styletron from 'styletron-client'
 import {StyletronProvider} from 'styletron-react'
 
-import * as firebase from 'firebase/app'
-import 'firebase/database'
-import 'firebase/auth'
-
 import Root from './components/Root'
 import i18n from './literals'
-import PlanDeGira from './domain'
+import domain, {fb} from './domain/instance'
 
-const config = {
-  apiKey: 'AIzaSyAO2KU0mgjMjhSkdqLnluZ1g3SxT-qvDgM',
-  authDomain: 'plandegira-359d8.firebaseapp.com',
-  databaseURL: 'https://plandegira-359d8.firebaseio.com',
-  projectId: 'plandegira-359d8',
-  storageBucket: 'plandegira-359d8.appspot.com',
-  messagingSenderId: '416314462852'
-}
-firebase.initializeApp(config)
-
-const domain = new PlanDeGira()
-domain.config('firebase', firebase)
-
-const render = Component => match({ routes, history: browserHistory }, (error, redirectLocation, renderProps) => {
-  if (error) { console.error({error}) }
-
+const render = Component =>
   ReactDOM.render(
     <AppContainer>
       <StyletronProvider styletron={new Styletron()}>
         <Provider i18n={i18n} domain={domain}>
-          <Router {...renderProps} />
+          <Router routes={routes} history={browserHistory} />
         </Provider>
       </StyletronProvider>
     </AppContainer>,
     document.getElementById('root')
   )
-})
 
-render(Root)
+fb.auth().onAuthStateChanged(user => {
+  console.log('UserApp', user)
+  render(Root)
+})
 
 if (module.hot) {
   module.hot.accept('./components/Root', () => {
     const NewRoot = require('./components/Root').default
-    render(NewRoot)
+    fb.auth().onAuthStateChanged(user => {
+      console.log('UserApp Hot', user)
+      render(NewRoot)
+    })
   })
 }
 
--- src/routes.js
+++ src/routes.js
@@ -2,19 +2,44 @@ import React from 'react'
 import Loadable from 'react-loadable'
 import Route from 'react-router/lib/Route'
 import Router from 'react-router/lib/Router'
-// import IndexRoute from 'react-router/lib/IndexRoute'
+import IndexRoute from 'react-router/lib/IndexRoute'
+
+import domain from './domain/instance'
 
 const Root = Loadable({loader: () => import(/* webpackChunkName: "Root" */ './components/Root'), loading: () => null})
 const Home = Loadable({loader: () => import(/* webpackChunkName: "Home" */ './pages/Home'), loading: () => null})
 const SignIn = Loadable({loader: () => import(/* webpackChunkName: "SignIn" */ './pages/SignIn'), loading: () => null})
 const SignUp = Loadable({loader: () => import(/* webpackChunkName: "SignUp" */ './pages/SignUp'), loading: () => null})
 
+const logout = async (nextState, replace, cb) => {
+  await domain.get('logout_users_use_case').execute()
+  replace('/signin')
+  return cb()
+}
+
+const requireAuth = async (nextState, replace, cb) => {
+  const user = await domain.get('current_users_use_case').execute()
+  console.log('User requireAuth', user) // eslint-disable-line
+  if (!user) { replace('/signin') }
+  return cb()
+}
+
+const redirectToHome = async (nextState, replace, cb) => {
+  const user = await domain.get('current_users_use_case').execute()
+  console.log('User redirectToHome', user) // eslint-disable-line
+  if (user) { replace('/') }
+  return cb()
+}
+
 export default (
   <Router>
     <Route component={Root}>
-      <Route path='/' component={Home} />
-      <Route path='/signup' component={SignUp} />
-      <Route path='/signin' component={SignIn} />
+      <Route path='/'>
+        <IndexRoute component={Home} onEnter={requireAuth} />
+        <Route path='signup' component={SignUp} onEnter={redirectToHome} />
+        <Route path='signin' component={SignIn} onEnter={redirectToHome} />
+        <Route path='logout' onEnter={logout} />
+      </Route>
     </Route>
   </Router>
 )
